e3bcdb29d914af5afc0846f555a3997df11924fe
Haseef Muhammed <haseefmuhammedpc@gmail.com>
2024-10-09 12:16:12 +0530

    final

--- script.js
+++ script.js
@@ -21,3 +21,26 @@ document.getElementById('playButton').addEventListener('click', function() {
       console.log('Play was prevented:', error);
   });
 });
+
+
+// IMG GALLER
+
+$('.portfolio-menu ul li').click(function(){
+  $('.portfolio-menu ul li').removeClass('active');
+  $(this).addClass('active');
+  
+  var selector = $(this).attr('data-filter');
+  $('.portfolio-item').isotope({
+    filter:selector
+  });
+  return  false;
+});
+$(document).ready(function() {
+var popup_btn = $('.popup-btn');
+popup_btn.magnificPopup({
+type : 'image',
+gallery : {
+  enabled : true
+}
+});
+});
